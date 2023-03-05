b23d697269cbfbff2f7cb43a9af3b0f4d6b7798d
Anit-G <kunalvartia@gmail.com>
2023-03-05 14:42:27 +0530

    add debug option and additional MFCC method

--- MFCC.py
+++ MFCC.py
@@ -22,7 +22,7 @@ def meltohertz(a,method='std'):
         return 700*(10**(a/2595)-1)
     return 1000*(10**(a*np.log10(2)/1000)-1)
 
-def mel_filterbank(N,n_filters=13,Fs=12000,min_freq=0,max_freq = None):
+def mel_filterbank(N,n_filters=13,Fs=12000,min_freq=0,max_freq = None, debug=False,method='m1'):
     """
     Compute a transformation matirx for mel filterbanks
 
@@ -37,12 +37,18 @@ def mel_filterbank(N,n_filters=13,Fs=12000,min_freq=0,max_freq = None):
             maximum filter frequency in Hz
         Fs:
             Sampling frequency of the signal
-    
+        debug:
+            print all the details of mel filterbank
+        method: value = m1 or m2
+            Find two methods to calculate mel filterbank transformation matrix
+            the calculate the filterbanks differently and give a slightly different filterbank
+            thus the results of the mel-spectrogram can be different for both I haven't checked.
+
+            Note: m2 method follows:
+                https://github.com/ddbourgin/numpy-ml/blob/b0359af5285fbf9699d64fd5ec059493228af03e/numpy_ml/preprocessing/dsp.py#L276
     Return:
         the transformation matrix of mel-filterbank
         Shape: (n_filters, N//2+1)
-
-    https://github.com/ddbourgin/numpy-ml/blob/b0359af5285fbf9699d64fd5ec059493228af03e/numpy_ml/preprocessing/dsp.py#L276
     """
 
     # define mel-filter parameters
@@ -52,38 +58,50 @@ def mel_filterbank(N,n_filters=13,Fs=12000,min_freq=0,max_freq = None):
     # Create the empty filterbank transformation matrix
     fbank = np.zeros((n_filters, N//2+1))
 
-    # ensure to scale the melscale to the size of spectrum N//2+1 as the they will be used for indexing
-    melscale_bins = np.floor(meltohertz(np.linspace(min_mel,max_mel,n_filters+2))/max_freq*(N//2+1)).astype(int)
-    melscale_spaces = np.diff(melscale_bins).astype(int)
+    if method=="m1":
+        """Method m1"""
+        # ensure to scale the melscale to the size of spectrum N//2+1 as the they will be used for indexing
+        melscale_bins = np.floor(meltohertz(np.linspace(min_mel,max_mel,n_filters+2))/max_freq*(N//2+1)).astype(int)
+        melscale_spaces = np.diff(melscale_bins).astype(int)
 
-    # Print Filterbank details:
-    print(f'\n\nFilterbank Details: \nmin_freq = {min_freq}, max_freq = {max_freq}')
-    print(f"MEL min = {min_mel}, MEL max = {max_mel}")
-    print(f"MEL Bins: {melscale_bins}")
-    print(f"MEL BW {melscale_spaces}")
+        for n in range(n_filters):
+            fbank[n,melscale_bins[n]:melscale_bins[n+1]] = np.linspace(0,1,melscale_spaces[n])
+            fbank[n,melscale_bins[n+1]:melscale_bins[n+2]] = np.linspace(1,0, melscale_spaces[n+1])
+    
+    elif method=='m2':
+        """Method m2"""
+        # sampling bins of the DFT
+        hzscale_bins = np.linspace(0, Fs/2, N//2+1,endpoint=True)
+        melscale_bins = meltohertz(np.linspace(min_mel,max_mel,n_filters+2))
+        melscale_spaces = np.diff(melscale_bins)
+        hzscale_bins = np.linspace(0,max_freq,N//2+1,endpoint=True)
 
-    # # sampling bins of the DFT
-    # hzscale_bins = np.linspace(0, Fs/2, N//2+1,endpoint=True)
+        fltrs = melscale_bins.reshape(-1,1)-hzscale_bins.reshape(1,-1)
+        for i in range(n_filters):
+            # Create the triangular filter at the correct mel frequency and bandwidth
+            # Calculate the filter value
+            Nl = -fltrs[i]/melscale_spaces[i]
+            Nr = fltrs[i+2]/melscale_spaces[i+1]
 
-    # fltrs = melscale_bins.reshape(-1,1)-hzscale_bins.reshape(1,-1)
-    # for i in range(n_filters):
-    #     # Create the triangular filter at the correct mel frequency and bandwidth
-    #     # Calculate the filter value
-    #     Nl = -fltrs[i]/melscale_spaces[i]
-    #     Nr = fltrs[i+2]/melscale_spaces[i+1]
+            fbank[i] = np.maximum(0,np.minimum(Nl,Nr))
+    else:
+        print("WARNING: argument method specified is incorrect")
+        return None
 
-    #     fbank[i] = np.maximum(0,np.minimum(Nl,Nr))
+    # Print Filterbank details: for debugging
+    if debug:
+        print(f'\n\nFilterbank Details: \nmin_freq = {min_freq}, max_freq = {max_freq}')
+        print(f"MEL min = {min_mel}, MEL max = {max_mel}")
+        print(f"Method used: \"{method}\"")
+        print(f"MEL Bins: {melscale_bins}")
+        print(f"MEL BW {melscale_spaces}")
 
-    for n in range(n_filters):
-        fbank[n,melscale_bins[n]:melscale_bins[n+1]] = np.linspace(0,1,melscale_spaces[n])
-        fbank[n,melscale_bins[n+1]:melscale_bins[n+2]] = np.linspace(1,0, melscale_spaces[n+1])
-
-    # Area normalization of triangular filters
+    # Area normalization of the triangular mel filters (done the same for both methods)
     enr_norm = 2/(melscale_bins[2:n_filters+2]-melscale_bins[:n_filters])
     fbank *= enr_norm[:,np.newaxis] 
     return fbank
 
-def Extract_Feature_Vec(link):
+def Extract_Feature_Vec(link, debug = False):
 
     # Get MFCC Vectors
         # window_length = 0.015 secs
@@ -97,10 +115,9 @@ def Extract_Feature_Vec(link):
     win_len = int(Fs*0.015)
     win_jump = int(Fs*0.005)
 
-    print(f'Computing MFCCs: \nWindow Length: {win_len}\nWindow Jump: {win_jump}\nNumber of Sample: {N}\nSampling Frequency: {Fs} Hz')
     win_sig = [sig[i:i+win_len] for i in range(0,N-win_len,win_jump)]
     num_wins = len(win_sig)
-    print(f'Number of windows: {num_wins}')
+    
     # Find energy of the signal
     E_sig = [i**2 for i in win_sig]
     E_sig = np.array(E_sig)
@@ -112,7 +129,7 @@ def Extract_Feature_Vec(link):
         # apply the filterbank on each windowed power sepctrum
         # multiply each filterbank spectrum with power spectrum 
     power_spec_sig = np.array([np.abs(DFT(s)/len(s))**2 for s in win_sig])
-    fbank = mel_filterbank(win_len,Fs=Fs)
+    fbank = mel_filterbank(win_len,Fs=Fs,debug=debug)
     mel_spectrogram = np.dot(fbank, power_spec_sig.T)
   
     # compute the MFCCs by logging the mel spectrogram and computing the DCT
@@ -127,23 +144,32 @@ def Extract_Feature_Vec(link):
     dE_sig = np.sum(dE_sig,axis=1)
     ddE_sig = np.sum(ddE_sig,axis=1)
 
-    print(f'MFCCs: \n{mfccs.shape}\n{dmfccs.shape}\n{ddmfccs.shape}')
-    print(f'Energy: \n{E_sig.shape}\n{dE_sig.shape}\n{ddE_sig.shape}')
-    
     # combine all and return the final feature vectors
     MFCCS_vector = np.zeros((42,num_wins))
     MFCCS_vector[:39,:] = np.vstack((mfccs,dmfccs,ddmfccs))
     MFCCS_vector[39:,:] = np.vstack((E_sig,dE_sig,ddE_sig))
 
-    print(f"MFCC Feature Vectors: {MFCCS_vector.shape}")
-
     # cepstral mean Substraction
     for i in range(MFCCS_vector.shape[0]):
         MFCCS_vector[i] -= np.mean(MFCCS_vector[i])
 
+    # For debugging
+    if debug:
+        print(f'Computing MFCCs: \nWindow Length: {win_len}\nWindow Jump: {win_jump}\nNumber of Sample: {N}\nSampling Frequency: {Fs} Hz')
+        print(f'Number of windows: {num_wins}')
+        print(f'MFCCs: \n{mfccs.shape}\n{dmfccs.shape}\n{ddmfccs.shape}')
+        print(f'Energy: \n{E_sig.shape}\n{dE_sig.shape}\n{ddE_sig.shape}')
+        print(f"MFCC Feature Vectors: {MFCCS_vector.shape}")
+
     return MFCCS_vector
 
-def MFCC_vecs_praat(path):
+def MFCC_vecs_praat(path,debug=False):
+    """
+    Using parselmouth calculate the MFCC feature vectors:
+        https://github.com/YannickJadoul/Parselmouth/blob/master/praat/dwtools/MFCC.h
+
+        contains the discription of MFCC functionality of parselmouth using praat
+    """
     sound = pm.Sound(path)
 
     # Find MFCC
@@ -152,20 +178,25 @@ def MFCC_vecs_praat(path):
     mfcc[0] = mfcc[0]**2                # configure c0 for energy
     dmfcc = delta(mfcc,2) 
     ddmfcc = delta(dmfcc,1)
-    print(f"\n\nPraat MFCCs: {mfcc.shape}, {dmfcc.shape}, {ddmfcc.shape}")
 
     # Combine all vectors:
     MFCC_vecs = np.vstack((mfcc[1:],dmfcc[1:],ddmfcc[1:],mfcc[0],dmfcc[0],ddmfcc[0]))
-    print(f"Final MFCC Vector Shape: {MFCC_vecs.shape}")
+
+    # For debugging:
+    if debug:
+        print(f"\n\nPraat MFCCs: {mfcc.shape}, {dmfcc.shape}, {ddmfcc.shape}")
+        print(f"Final MFCC Vector Shape: {MFCC_vecs.shape}")
+
     return MFCC_vecs
 
 
 if __name__ == '__main__':
+    debug = False
     sample_link = './Data/Dev/Team-05/111a.wav'
-    fv = Extract_Feature_Vec(sample_link)
+    fv = Extract_Feature_Vec(sample_link,debug=debug)
     fs, sig = w.read(sample_link)
-    filters = mel_filterbank(300,Fs=fs)
-    p_fv = MFCC_vecs_praat(sample_link)
+    p_fv = MFCC_vecs_praat(sample_link,debug=debug)
+    filters = mel_filterbank(300,Fs=fs,debug=debug)
 
     # Data presentation:
     fig,ax = plt.subplots(2,2,figsize=(15,15))
